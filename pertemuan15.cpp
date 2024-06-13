820a167c03d7b3a2cc33ba02f3af8de347e62f42
Viaasyafrina16 <syafrinavia@gmail.com>
2024-06-13 16:08:14 +0700

    membuat case 2

--- pertemuan15.cpp
+++ pertemuan15.cpp
@@ -143,6 +143,11 @@ public:
                 x.insert(word);
                 break;
             }
+            case '2':
+            {
+                x.inorder(x.ROOT);
+                break;
+            }
             
             }
 
