9530501dbe8fd6b97f81bd16a41b8ce71957ac63
Viaasyafrina16 <syafrinavia@gmail.com>
2024-06-13 15:31:38 +0700

    menambahkan kondisi jika nilai element lebih kecil dari parent

--- pertemuan15.cpp
+++ pertemuan15.cpp
@@ -30,8 +30,29 @@ public:
 
     void insert(string element) // Insert a node in the binary search tree
     {
-        Node* newNode = new Node(element, nullptr, nullptr);
+        Node* newNode = new Node(element, nullptr, nullptr); // allocate memory for the new node
+        newNode->info = element; // Assign value to the data field of the new node
+        newNode->leftchild = nullptr; // Make the left child of the new node point to NULL
+        newNode->rightchild = nullptr; // make the right child of the new node point to NULL
 
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); // Locate the node which will be the parent of the node to be insert
+
+        if (parent == nullptr)// if the parent is NULL (Tree is empty)
+        {
+            ROOT = newNode; // Mark the new node as ROOT
+            return; // Exit
+        }
+
+        if (element < parent->info) // if the value in the data field of the new node is less than that of the
+        {
+            parent->leftchild = newNode; // Make the left child of the parent point to the new node
+        }
+        
+    
+    
+    
     }
 
 
